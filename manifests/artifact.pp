c2b7517021a799a75b166991d3a2a8140b928863
Scott Stevelinck <scott@fit-pay.com>
2016-08-01 07:47:00 -0600

    set classifier to undef and lots of little style fixes

--- manifests/artifact.pp
+++ manifests/artifact.pp
@@ -3,17 +3,23 @@
 # This resource downloads Maven Artifacts from Artifactory
 #
 # Parameters:
-# [*ensure*] : If 'present' checks the existence of the output file (and downloads it if needed), if 'absent' deletes the output file, if not set redownload the artifact
+# [*ensure*] : If 'present' checks the existence of the output file (and
+# downloads it if needed), if 'absent' deletes the output file, if not set
+# redownload the artifact
 # [*gav*] : The artifact groupid:artifactid:version (mandatory)
 # [*packaging*] : The packaging type (jar by default)
 # [*classifier*] : The classifier (no classifier by default)
-# [*repository*] : The repository such as 'public', 'central'... (defaults to 'releases' or 'snapshots' depending on the version specified in gav
+# [*repository*] : The repository such as 'public', 'central'... (defaults to
+# 'releases' or 'snapshots' depending on the version specified in gav
 # [*output*] : The output file (defaults to the resource name)
 #
 # Actions:
 # If repository is set, its setting will be honoured.
-# If repository is not set, its value is derived from the version contained in *gav*.  If the *gav* version is a SNAPSHOT then the repository will be set to 'snapshots', otherwise it will be 'releases'.
-# If ensure is set to 'present' the resource checks the existence of the file and download the artifact if needed.
+# If repository is not set, its value is derived from the version contained in
+# *gav*.  If the *gav* version is a SNAPSHOT then the repository will be set to
+# 'snapshots', otherwise it will be 'releases'.
+# If ensure is set to 'present' the resource checks the existence of the file
+# and download the artifact if needed.
 # If ensure is set to 'absent' the resource deleted the output file.
 # If ensure is not set or set to 'update', the artifact is re-downloaded.
 #
@@ -44,10 +50,10 @@
 #   }
 #
 define artifactory::artifact(
-  $ensure = update,
   $gav,
+  $ensure = update,
   $packaging = 'jar',
-  $classifier = '',
+  $classifier = undef,
   $repository = '',
   $output = $name,
   $timestamped = false)
@@ -72,7 +78,7 @@ define artifactory::artifact(
   }
 
   if ($timestamped) {
-    $timestampedRepo = "-t"
+    $timestampedRepo = '-t'
   }
 
   $cmd = "/opt/artifactory-script/download-artifact-from-artifactory.sh -a ${gav} -e ${packaging} ${includeClass} -n ${artifactory::artifactory_url} ${includeRepo} ${timestampedRepo} -o ${output} ${args} -v"
@@ -84,8 +90,8 @@ define artifactory::artifact(
     }
   } elsif $ensure == absent {
     file { "Remove ${gav}-${classifier} to ${output}":
-      path   => $output,
-      ensure => absent
+      ensure => absent,
+      path   => $output
     }
   } else {
     exec { "Download ${gav}-${classifier} to ${output}":
